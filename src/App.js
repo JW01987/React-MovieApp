f9bdc81b519b3a4eeab763f2e79870ed441e4a43
Jean <etnpfj@gmail.com>
2023-01-19 14:02:49 +0900

    가볍게 리액트로  todo구현해보기"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  let [toDo, setToDo] = useState("");
+  let [toDos, setToDos] = useState([]);
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (toDo === "") return;
+    setToDos((c) => [toDo, ...c]);
+    setToDo("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My To Do({toDos.length})</h1>
+      <div>
+        <form>
+          <input type="text" value={toDo} onChange={onChange} />
+          <button onClick={addTodo}>✅Add</button>
+        </form>
+      </div>
+      <hr />
+      <ul>
+        {toDos.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 }
