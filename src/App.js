b5908187eeca1ec141ec963e19fd3a84724f5b67
Jean <etnpfj@gmail.com>
2023-01-19 19:44:06 +0900

    코인API를 가져와 달러/코안 변환기 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,71 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  let [toDo, setToDo] = useState("");
-  let [toDos, setToDos] = useState([]);
+  let [coins, setCoins] = useState([]);
+  let [loading, setLoading] = useState(true);
+  let [checkCoin, setCheckCoin] = useState(0);
+  let [inputValue, setInputValue] = useState(0);
+  let [btn, setBtn] = useState(true);
   const onChange = (e) => {
-    setToDo(e.target.value);
+    setCheckCoin(parseFloat(e.target.value));
   };
-  const addTodo = (e) => {
-    e.preventDefault();
-    if (toDo === "") return;
-    setToDos((c) => [toDo, ...c]);
-    setToDo("");
-  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
+  const inputChange = (e) => {
+    setInputValue(parseFloat(e.target.value) || "");
+  };
   return (
     <div className="App">
-      <h1>My To Do({toDos.length})</h1>
-      <div>
-        <form>
-          <input type="text" value={toDo} onChange={onChange} />
-          <button onClick={addTodo}>✅Add</button>
-        </form>
-      </div>
+      <h1>The Coins{loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChange}>
+          <option>Select your coin</option>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name}({coin.symbol}):{coin.quotes.USD.price}USD
+            </option>
+          ))}
+        </select>
+      )}
       <hr />
-      <ul>
-        {toDos.map((item, i) => {
-          return <li key={i}>{item}</li>;
-        })}
-      </ul>
+
+      <label htmlFor="usd">USD</label>
+      <input
+        id="usd"
+        type="number"
+        onChange={inputChange}
+        disabled={btn}
+        value={
+          btn ? parseFloat(inputValue * checkCoin) : parseFloat(inputValue)
+        }
+      />
+
+      <label htmlFor="toCoin">Coin</label>
+      <input
+        id="toCoin"
+        type="number"
+        onChange={inputChange}
+        disabled={!btn}
+        value={
+          !btn ? parseFloat(inputValue * checkCoin) : parseFloat(inputValue)
+        }
+      />
+      <button
+        onClick={() => {
+          setBtn((c) => !c);
+        }}
+      >
+        Change
+      </button>
     </div>
   );
 }
