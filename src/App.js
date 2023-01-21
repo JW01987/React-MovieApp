bb3c7b72e9032ad501c05358a527b27cd345ed1e
Jean <etnpfj@gmail.com>
2023-01-21 15:45:51 +0900

    깃허브 페이지 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Home from "./routes/Home";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
--- src/routes/Genre.js
+++ src/routes/Genre.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 import { useParams } from "react-router-dom";
